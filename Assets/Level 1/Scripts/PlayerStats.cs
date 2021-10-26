ca91ce1cef2b032b531e6c625b52dc01b4b4587b
deep <pardeep.kumar@studenti.unipd.it>
2021-10-26 22:23:05 +0200

    Added the possibility to gather coin
    Added the possibility to gather coin

--- Assets/Level 1/Scripts/PlayerStats.cs
+++ Assets/Level 1/Scripts/PlayerStats.cs
@@ -9,14 +9,20 @@ public class PlayerStats : MonoBehaviour
     private int _stamina = PlayerMaxStamina;
     private const int PlayerMaxStamina=200;
     private const int PlayerMaxHealth=200;
+    private int _coins=1;
 
     private HealthBarScript healthBarScript;
     private StaminaBarScript staminaBarScript;
+    private CoinCounterTextScript _coinCounterTextScript;
 
     private void Start()
     {
         healthBarScript = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<HealthBarScript>();
         staminaBarScript=GameObject.FindGameObjectWithTag("StaminaBar").GetComponent<StaminaBarScript>();
+        _coinCounterTextScript=GameObject.FindGameObjectWithTag("CoinsAmount").GetComponent<CoinCounterTextScript>();
+
+        
+        _coinCounterTextScript.setCoinsAmount(_coins);
         healthBarScript.SetMaxHealth(PlayerMaxHealth);
         staminaBarScript.SetMaxStamina(PlayerMaxStamina);
         Debug.Log(healthBarScript);
@@ -42,5 +48,15 @@ public class PlayerStats : MonoBehaviour
     public int getStamina()
     {
         return _stamina;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            _coins++;
+            _coinCounterTextScript.setCoinsAmount(_coins);
+            Destroy(other.gameObject);
+        }
     }
 }
