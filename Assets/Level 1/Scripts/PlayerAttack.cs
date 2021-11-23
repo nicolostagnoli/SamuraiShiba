7db69e0f2f8663ee935fd538758da8d4f85f0db6
nicolostagnoli <61749537+nicolostagnoli@users.noreply.github.com>
2021-11-23 18:17:31 +0100

    Revert "Revert "Updated Crane Paths""
    This reverts commit 9aa229c7f8ed9c6c96e43737afb61d39d2615c84.

--- Assets/Level 1/Scripts/PlayerAttack.cs
+++ Assets/Level 1/Scripts/PlayerAttack.cs
@@ -8,6 +8,7 @@ public class PlayerAttack : MonoBehaviour
     private Animator _animator;
     private int _comboCont;
     private float _timeBetweenCombo;
+    private PlayerStats _playerStats;
 
     public float startTimeBetweenLightAttacks;
     public float startTimeBetweenHeavyAttacks;
@@ -18,6 +19,8 @@ public class PlayerAttack : MonoBehaviour
     public float heavyDamage;
     public float comboDuration;
     public float comboDamageBoost;
+    public float lightAttackStamina;
+    public float heavyAttackStamina;
 
     // Start is called before the first frame update
     private void Start() {
@@ -25,6 +28,7 @@ public class PlayerAttack : MonoBehaviour
         _timeBetweenCombo = 0;
         _comboCont = 0;
         _animator = GetComponent<Animator>();
+        _playerStats = GetComponent<PlayerStats>();
     }
 
     // Update is called once per frame
@@ -33,51 +37,57 @@ public class PlayerAttack : MonoBehaviour
         if(_timeBetweenAttacks <= 0) {
             //LIGHT ATTACK
            if (Input.GetKey(KeyCode.N)) {
-                Debug.Log("Light Attacking");
-                Collider2D[] enemiesToAttack = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemy);
-                for(int i = 0; i < enemiesToAttack.Length; i++) {
-                    enemiesToAttack[i].gameObject.GetComponent<Enemy>().TakeDamage(lightDamage+_comboCont*comboDamageBoost);
-                    Debug.Log("Damage " + lightDamage+_comboCont*comboDamageBoost + " to: " + enemiesToAttack[i].gameObject.name);
-                    //combo counter
-                    if(_timeBetweenCombo > 0){
-                        _comboCont++;
-                    }else{
-                        _comboCont = 1;
-                    }
-                    Debug.Log(_comboCont + " Hit!");
-                    _timeBetweenCombo = comboDuration;
+                if (_playerStats.getStamina() >= lightAttackStamina) {
+                    _playerStats.UseStamina((int)lightAttackStamina);
+                    _timeBetweenAttacks = startTimeBetweenLightAttacks;
+                    _animator.SetTrigger("LightAttack");
                 }
-
-                _timeBetweenAttacks = startTimeBetweenLightAttacks;
-                _animator.SetTrigger("LightAttack");
 
             } //HEAVY ATTACK
             else if (Input.GetKey(KeyCode.M)) {
-                Debug.Log("Heavy Attacking");
-                Collider2D[] enemiesToAttack = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemy);
-                for(int i = 0; i < enemiesToAttack.Length; i++) {
-
-                    enemiesToAttack[i].gameObject.GetComponent<Enemy>().TakeDamage(heavyDamage+_comboCont*comboDamageBoost*1.2f);
-                    Debug.Log("Damage " + heavyDamage+_comboCont*comboDamageBoost*1.2f + " to: " + enemiesToAttack[i].gameObject.name);
-
-                    //Combo counter
-                     if(_timeBetweenCombo > 0){
-                        _comboCont++;
-                    }else{
-                        _comboCont = 1;
-                    }
-                    Debug.Log(_comboCont + " Hit!");
-                    _timeBetweenCombo = comboDuration;
+                if (_playerStats.getStamina() >= heavyAttackStamina) {
+                    _playerStats.UseStamina((int)heavyAttackStamina);
+                    _timeBetweenAttacks = startTimeBetweenHeavyAttacks;
+                    _animator.SetTrigger("HeavyAttack");
                 }
-
-                _timeBetweenAttacks = startTimeBetweenHeavyAttacks;
-                _animator.SetTrigger("HeavyAttack");
             }
         }
         else {
             _timeBetweenAttacks -= Time.deltaTime;
         }
         _timeBetweenCombo -= Time.deltaTime;
+    }
+
+    public void MakeLightAttack() {
+        Collider2D[] enemiesToAttack = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemy);
+        for (int i = 0; i < enemiesToAttack.Length; i++) {
+            enemiesToAttack[i].gameObject.GetComponent<Enemy>().TakeDamage(lightDamage + _comboCont * comboDamageBoost);
+            //combo counter
+            if (_timeBetweenCombo > 0) {
+                _comboCont++;
+            }
+            else {
+                _comboCont = 1;
+            }
+            Debug.Log(_comboCont + " Hit!");
+            _timeBetweenCombo = comboDuration;
+        }
+    }
+
+    public void MakeHeavyAttack() {
+        Collider2D[] enemiesToAttack = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemy);
+        for (int i = 0; i < enemiesToAttack.Length; i++) {
+            enemiesToAttack[i].gameObject.GetComponent<Enemy>().TakeDamage(heavyDamage + _comboCont * comboDamageBoost * 1.2f);
+            //Combo counter
+            if (_timeBetweenCombo > 0) {
+                _comboCont++;
+            }
+            else {
+                _comboCont = 1;
+            }
+            Debug.Log(_comboCont + " Hit!");
+            _timeBetweenCombo = comboDuration;
+        }
     }
 
     private void OnDrawGizmosSelected() {
