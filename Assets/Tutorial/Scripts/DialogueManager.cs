758ae60eff5b30d95af3a10b10dd63f670f115f2
Davideub <davide.ubaldi@outlook.it>
2021-11-23 16:59:33 +0100

    Turorial with base dialogs

---
+++ Assets/Tutorial/Scripts/DialogueManager.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogueText;
+
+	public Animator animator;
+
+	private Queue<string> sentences;
+	public DialogueTrigger dialogueTrigger;
+	
+
+	// Use this for initialization
+	void Start () {
+		sentences = new Queue<string>();
+		dialogueTrigger.TriggerDialogue();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			DisplayNextSentence();
+		}
+	}
+
+	public void StartDialogue (Dialogue dialogue)
+	{
+		animator.SetBool("IsOpen", true);
+
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence ()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence (string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
+
+	void EndDialogue()
+	{
+		animator.SetBool("IsOpen", false);
+	}
+
+}
