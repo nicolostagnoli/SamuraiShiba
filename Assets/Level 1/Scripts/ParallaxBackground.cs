8fd7dbf84f7a338a51a68d79cec57c02ecec6b2a
Davideub <davide.ubaldi@outlook.it>
2021-12-04 12:30:05 +0100

    Background_Ground fix

---
+++ Assets/Level 1/Scripts/ParallaxBackground.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField]
+private Transform cameraTransform;
+
+private Vector3 lastCameraPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        float parallaxEffectMultiplier = .9f;
+        transform.position += deltaMovement * parallaxEffectMultiplier;
+        lastCameraPosition = cameraTransform.position;
+    }
+}
