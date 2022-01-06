6e550bb730d590e52ffd4c678d699be67551f8ba
zzizaa <gabriele.ziza@gmail.com>
2022-01-06 18:42:15 +0100

    change jump when defeat crane

--- Assets/Persistent/Scripts/StateNameController.cs
+++ Assets/Persistent/Scripts/StateNameController.cs
@@ -6,5 +6,6 @@ public class StateNameController : MonoBehaviour
 {
     public static float playerHealth;
     public static float playerStamina;
+    public static float jumpForce = 8f;
     public static Inventory playerInventory;
 }
