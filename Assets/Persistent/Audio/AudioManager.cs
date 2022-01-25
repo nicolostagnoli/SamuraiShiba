f68d0a8b39ebf57928585e885a34a16c71b02713
nicolostagnoli <61749537+nicolostagnoli@users.noreply.github.com>
2022-01-25 18:06:45 +0100

    Revert "boss health"
    This reverts commit 74f45596b8f522b5dd96cf9ae2f468a6faae9df7.

--- Assets/Persistent/Audio/AudioManager.cs
+++ Assets/Persistent/Audio/AudioManager.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour {
     public static AudioManager singleton;
     private AudioSource _audioSource;
-    [SerializeField]
-    public Queue<AudioClip> clipQueue;
-
     private void Awake() {
         
         if (singleton != null)
@@ -21,16 +18,6 @@ public class AudioManager : MonoBehaviour {
         }
         
         _audioSource = GetComponent<AudioSource>();
-    }
-
-    void Update() {
-        if (_audioSource.isPlaying == false && clipQueue.Count > 0) {
-            _audioSource.clip = clipQueue.Dequeue();
-            _audioSource.Play();
-        }
-    }
-    public void PlaySound(AudioClip clip) {
-        clipQueue.Enqueue(clip);
     }
 
     public void PlayMusic() {
