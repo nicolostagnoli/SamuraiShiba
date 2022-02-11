d60d2766977209cd13826cb18346c8332dd1160b
Davideub <davide.ubaldi@outlook.it>
2022-02-11 17:04:50 +0100

    fix

--- Assets/MenuManager/Scripts/PlayerInterface.cs
+++ Assets/MenuManager/Scripts/PlayerInterface.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,6 +44,11 @@ using UnityEngine;
         {
             
             MenuManager.Instance.SetActiveSpecificMenu(GameOverMenu.Instance);
+        }
+
+        public void OnDisable()
+        {
+            Resume();
         }
     }
 
